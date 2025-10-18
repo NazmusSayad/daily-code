c40696b957a56e6e1c4f5fa23e5cfacf7be6a7a6
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-10-18 14:56:09 +0600

    Update package version to 1.1.3 and refactor rollup configuration to utilize a createOutput function for cleaner output generation across multiple formats.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,13 @@
 import typescript from '@rollup/plugin-typescript'
-import { defineConfig } from 'rollup'
+import { defineConfig, type ModuleFormat } from 'rollup'
 import dts from 'rollup-plugin-dts'
 
 const inputs = {
   index: 'src/index.ts',
-  types: 'src/types/index.ts',
+
   node: 'src/node/index.ts',
   server: 'src/server/index.ts',
+
   browser: 'src/browser/index.ts',
   react: 'src/react/index.ts',
 } as const
@@ -15,23 +16,10 @@ export default defineConfig([
   {
     input: inputs,
     output: [
-      {
-        dir: 'dist',
-        format: 'es',
-        sourcemap: true,
-        entryFileNames({ name }) {
-          return `${name}.mjs`
-        },
-      },
-
-      {
-        dir: 'dist',
-        format: 'cjs',
-        sourcemap: true,
-        entryFileNames({ name }) {
-          return `${name}.cjs`
-        },
-      },
+      createOutput({ format: 'esm', ext: 'mjs', sourcemap: true }),
+      createOutput({ format: 'commonjs', ext: 'cjs', sourcemap: true }),
+      createOutput({ format: 'amd', ext: 'amd.js', sourcemap: true }),
+      createOutput({ format: 'system', ext: 'system.js', sourcemap: true }),
     ],
 
     external: [/node_modules/],
@@ -40,14 +28,26 @@ export default defineConfig([
 
   {
     input: inputs,
-    output: {
-      dir: 'dist',
-      format: 'es',
-      entryFileNames({ name }) {
-        return `${name}.d.ts`
-      },
-    },
+    output: createOutput({ format: 'esm', ext: 'd.ts', sourcemap: false }),
 
     plugins: [dts({ tsconfig: './tsconfig.json' })],
   },
 ])
+
+function createOutput(options: {
+  format: ModuleFormat
+  ext: string
+  sourcemap: boolean
+}) {
+  return {
+    dir: 'dist',
+    indent: '  ',
+
+    format: options.format,
+    sourcemap: options.sourcemap,
+
+    entryFileNames({ name }) {
+      return `${name}.${options.ext}`
+    },
+  }
+}
